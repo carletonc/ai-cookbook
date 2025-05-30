d4bff09e0592dd84e9cb7cdae0f645c2d7356feb
CCoggins <carleton.coggins@gmail.com>
2025-05-29 21:35:04 -0500

    .

---
+++ src/prompt_tuning/constants.py
@@ -0,0 +1,19 @@
+DESCRIPTION = """
+This app is a tempalte that demonstrates an semi-supervised way to automate prompt tuning inspired by neural network training.
+
+Our initial prompt follows basic prompt engineering techniques, such as giving the LLM an identity, a task, some context data, and instructions.
+
+The evaluation prompts are designed to evaluate the LLMs output across different verticals, and follow a more formal structure of
+an identity, context, task, scoring rubric with output examples, output format, along with the related data to evaluate (input prompt, data, & LLM output).
+
+These are general evaluation prompts to be adapted to any use, and can be extended on for specific domains (Marketing, Finance, etc.).
+
+Our tuning prompt is the most formal and rigid, as is necessary for evaluating and improving upon the input prompt.
+"""
+
+dummy_data = """
+Quarter,Revenue ($M),Profit Margin (%),Customer Satisfaction (%),Enterprise Growth (%),Consumer Growth (%)
+Q1 2024,4.1,12.5,85,38,-8
+Q2 2024,4.3,13.1,87,41,-5
+Q3 2024,5.18,15.3,72,45,-12
+"""
